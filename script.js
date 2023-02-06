cfd68088d822ee13c985f607f05238fbeba9b796
DaniilVat <malouw211@gmail.com>
2023-02-06 16:49:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// Задача 1 
+
+//const wewq = function (num1, num2, num3) {
+  //  return (num1 -  num2) / num3
+//}
+
+// Задача 2 
+
+//const kyb = function(number){
+   //return number * number
+
+//}
+
+//const result = kyb(20)
+
+//console.log(result)
+
+//const kvadrat = function (number) {
+ // return number*number*number
+//}
+//const result2 = kvadrat(30)
+//console.log(result2)
+
+// Задача 3
+
+//function min (a, b) {
+//if (a < b) {
+  //  return a
+//} else {
+ //   return b
+//}
+//}
+
+//const result = min (2, 1);
+//console.log(result)
+
+// Задача 5
+
+const isEven = function (number) {
+    if (number % 2 === 0 ) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+
+}
+const result = isEven (2)
+
+// Задача 6
+
+var arr = [0,4,7,3,5,6,9,5,4,7,8,3,2,4,56,6,8,69];
+     var newArr = [];
+     for (let i = 0 ; i < arr.length, i++;) {
+      if  (isEven(i)) {
+        newArr.push(i)
+      }
+      
+     }
+
+     console.log(newArr)
+
+
+     
+     
+
+
+
+
+
